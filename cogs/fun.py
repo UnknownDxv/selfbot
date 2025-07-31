c7ca79463e90cda41f6415813b1c201924f79508
UnknownDxv <piyushxyz143@gmail.com>
2025-07-31 23:09:59 +0530

    add spam command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+import asyncio
+from discord import HTTPException
 from discord.ext import commands
 from discord.ext.commands import Cog
 from core.context import CustomContext
@@ -41,6 +43,30 @@ class FunCog(Cog):
         answer = random.choice(responses)
         await ctx.delete()
         await ctx.send(f"🎱 **Question:** {question} **Answer:** {answer}")
+
+    @commands.command(
+        name="spam",
+        description="Spam  a message multiple times.",
+        aliases=['sp'],
+        usage="<message> <amount> <delay>"
+    )
+    async def spam(self, ctx: CustomContext, amount: int, delay: int, * , message: str) -> None:
+        '''Spam a message multiple times.'''
+        if not (3 <= amount <= 100):
+            return await ctx.edit_or_send("Amount must be between 3 and 100.", delete_after=5)
+        if delay < 0:
+            return await ctx.edit_or_send("Delay cannot be negative.", delete_after=5)
+        
+        await ctx.delete()
+        for _ in range(amount):
+            try:
+                await ctx.send(message)
+                await asyncio.sleep(delay)
+            except HTTPException as e:
+                if e.status == 429:
+                    retry_after = getattr(e, "retry_after", 5)
+                    await asyncio.sleep(retry_after)
+                    await ctx.send(message)
 
 
 async def setup(client: SelfBot) -> None:
